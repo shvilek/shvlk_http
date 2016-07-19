0014b98fdc082a05aeac57058f5ea80978070b87
asuhov <ashuov@corp.sputnik.ru>
2016-07-19 15:57:49 +0300

    asd

--- cserver.cpp
+++ cserver.cpp
@@ -38,7 +38,68 @@ int CServer::run(const std::string& aHost, const std::string& aDir, int aPort) {
     listen(master_, SOMAXCONN);
     setnonblocking(master_);
 
+    sockaddr_in addr;
+    socklen_t addrlen = sizeof(addr);
+
     int client_ = 0;
+#define MAX_EVENTS 10
+    struct epoll_event ev, events[MAX_EVENTS];
+               int epollfd = epoll_create1(0);
+               if (epollfd == -1) {
+                   perror("epoll_create1");
+                   exit(EXIT_FAILURE);
+               }
+
+               ev.events = EPOLLIN;
+               ev.data.fd = master_;
+               if (epoll_ctl(epollfd, EPOLL_CTL_ADD, master_, &ev) == -1) {
+                   perror("epoll_ctl: Master");
+                   exit(EXIT_FAILURE);
+               }
+
+               while  (running()) {
+                   int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+                   if (nfds == -1) {
+                       perror("epoll_wait");
+                       exit(EXIT_FAILURE);
+                   }
+
+                   for (int n = 0; n < nfds; ++n) {
+                       if (events[n].data.fd == master_) {
+                           int conn_sock = accept(master_, (sockaddr *) &addr, &addrlen);
+                           if (conn_sock == -1) {
+                               perror("accept");
+                               exit(EXIT_FAILURE);
+                           }
+                           setnonblocking(conn_sock);
+                           push_request(conn_sock);
+                           /*for (auto i = clients_.begin(); i != clients_.end();++i) {
+                               //sockaddr_in* sa = (sockaddr_in*)&addr;
+                               char *ip = inet_ntoa(addr.sin_addr);
+                                int sc = 0;
+                                if (*i == conn_sock) {
+                                    sc = send(*i, "Hello!", 6, MSG_NOSIGNAL);
+                                } else {
+                                    sc = send(*i, ip, strlen(ip), MSG_NOSIGNAL);
+                                }
+
+                                if (sc <= 0) {
+                                   shutdown(*i, SHUT_RDWR);
+                                   clients_.erase(*i);
+                                }
+                           }
+
+                           ev.events = EPOLLIN | EPOLLET;
+                           ev.data.fd = conn_sock;
+                           if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,
+                                       &ev) == -1) {
+                               perror("epoll_ctl: conn_sock");
+                               exit(EXIT_FAILURE);
+                           }*/
+                       }
+                   }
+               }
+    /*
     timeval tv;
         tv.tv_sec = 1;
         tv.tv_usec = 0;
@@ -53,7 +114,7 @@ int CServer::run(const std::string& aHost, const std::string& aDir, int aPort) {
                }
            }
        }
-
+*/
        clear_all();
        return 0;
 }
