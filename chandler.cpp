5c3ac72b1c069476fb8477c0b8d436bbb14c2074
asuhov <ashuov@corp.sputnik.ru>
2016-07-19 13:58:38 +0300

    123

--- chandler.cpp
+++ chandler.cpp
@@ -141,7 +141,7 @@ void CHandler::process_request(int aConnection) {
                     auto now = std::chrono::system_clock::now();
                     auto now_c = std::chrono::system_clock::to_time_t(now);
 
-                    ss << "HTTP/1.0 200 OK\r\n\r\n\r\n";
+                    ss << "HTTP/1.0 200 OK\r\n\r\n";
                     //ss << "Date: " << std::put_time(std::localtime(&now_c), "%c") << "\r\n";
                     //ss << "Content-Type: text/html\r\n";
                     //ss << "Content-Length: " << size_  << "\r\n" << "\r\n";
